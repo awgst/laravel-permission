847e0debee3bb938657be4e714115ac5cb495073
awgst <trisaktiawang@gmail.com>
2021-11-25 06:52:43 +0700

    show all post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view('post', compact('posts'));
     }
 
     /**
---
+++ resources/views/post.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <a href="#" class="btn btn-primary mx-2 mb-2">Tambah</a>
+                    </div>
+                    <div class="list-group mb-2">
+                      @foreach($posts as $post)
+                      <div class="list-group-item list-group-item-action mb-2">
+                        <div class="d-flex">
+                            <div class="description w-100 mx-3">
+                                <div class="d-flex w-100 justify-content-between">
+                                  <a href="{{ route('post.show', $post->id) }}" class="text-decoration-none text-dark"><h5 class="mb-1">{{ $post->title }}</h5></a>
+                                  <small class="d-flex">
+                                    <a href="{{ route('post.edit', $post->id) }}" class="btn btn-success mx-1">Edit</a>
+                                    <form action="{{ route('post.destroy', $post->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger mx-1">Delete</button>
+                                    </form>
+                                  </small>
+                                </div>
+                                <small class="text-muted">{{ $post->body }}</small>        
+                            </div>
+                        </div>
+                      </div>
+                      @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
